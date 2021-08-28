597eae44446816bd6080f3ea6f1e0d293c802af4
IM Arifullah <arifullah_360@micple.com>
2021-08-28 16:44:01 +0600

    fdfd

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT ||3000
+const app = express();
+const publicDirectory = path.join(__dirname, 'build');
+app.use(express.static(publicDirectory));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicDirectory, 'index.html'));
+});
+app.use((req, res) => {
+  res.status(404).json({ message: 'Request not found.' });
+});
+
+app.listen(port, () => console.log('Web server started.'+port));
